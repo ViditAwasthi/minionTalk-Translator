94e5803ad8ec05d807d63bb6238a605f70aefd3e
Vidit Awasthi <viditawasthi2@gmail.com>
2021-07-31 01:43:52 +0530

    added css

--- app.js
+++ app.js
@@ -3,21 +3,23 @@ var txtInput = document.querySelector("#txt-input");
 var divOutput = document.getElementById("div-output");
 var url = "	https://api.funtranslations.com/translate/minion.json"
 
-function constructURL(text){
-    return url +"?text="+text
+function constructURL(text) {
+    return url + "?text=" + text
 }
 
-function handleError(error){
+function handleError(error) {
     console.log("error occured", error);
 }
 
-btnTranslate.addEventListener("click", function clickEventHandler(){  
+btnTranslate.addEventListener("click", function clickEventHandler() {
 
     var constructedURL = constructURL(txtInput.value);
 
     fetch(constructedURL)
-    .then(response => response.json())
-    .then(json => {divOutput.innerText = json.contents.translated})
-    .catch(handleError)
-    
+        .then(response => response.json())
+        .then(json => {
+            divOutput.innerText = json.contents.translated
+        })
+        .catch(handleError)
+
 })
